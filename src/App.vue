5b92b79c8344a9f0f5b05639742dd8e1d0beb842
jason06286 <dj4871114@gmail.com>
2022-08-18 14:48:12 +0800

    chore: env init

---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<script setup>
+import HelloWorld from "./components/HelloWorld.vue";
+import Home from "./pages/Home.vue";
+</script>
+<template>
+  <Home />
+</template>
+<style></style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,5 @@
+<script setup></script>
+<template>
+  <div class="text-[100px] text-blue-500">123</div>
+</template>
+<style></style>
